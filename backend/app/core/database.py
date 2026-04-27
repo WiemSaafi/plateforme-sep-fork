33ec7f9db0fee3689d3acd354a2ed974c8ecbee9
wiem saafi <164183377+WiemSaafi@users.noreply.github.com>
2026-04-27 16:48:59 +0300

    Refactor database connection and fix MongoDB URL  database modifierrr
    Refactor database connection to use a global motor_client and update MongoDB URL.  
    database modifierr

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,13 +5,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://mllehadillajili_db_user:Sq7ov3CoE4lVRSaN@sepcluster.woh3fey.mongodb.net/sep_db?appName=SepCluster")
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://mllehadillajili_db_user:Sq7ov3CoE4lVRRSaN@sepcluster.woh3fey.mongodb.net/sep_db?appName=SepCluster")
+
+# Client global réutilisable
+motor_client: AsyncIOMotorClient = None
 
 async def connect_db():
-    client = AsyncIOMotorClient(MONGODB_URL)
-    from app.models.documents import Patient, VisiteClinique, IRMScan, Utilisateur, AnalyseBiologique, Rappel, RendezVous, Contrat
-    await init_beanie(
-        database=client.sep_db,
-        document_models=[Patient, VisiteClinique, IRMScan, Utilisateur, AnalyseBiologique, Rappel, RendezVous, Contrat]
+    global motor_client
+    motor_client = AsyncIOMotorClient(MONGODB_URL)
+    from app.models.documents import (
+        Patient, VisiteClinique, IRMScan, Utilisateur,
+        AnalyseBiologique, Rappel, RendezVous, Contrat
     )
-    print("Connecte a MongoDB")
+    await init_beanie(
+        database=motor_client.sep_db,
+        document_models=[
+            Patient, VisiteClinique, IRMScan, Utilisateur,
+            AnalyseBiologique, Rappel, RendezVous, Contrat
+        ]
+    )
+    print("Connecté à MongoDB")
+
+def get_db():
+    return motor_client.sep_db
