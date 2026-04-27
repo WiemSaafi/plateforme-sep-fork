4994a6ca21a13d9838b8a05563fbb23f66302793
wiem saafi <164183377+WiemSaafi@users.noreply.github.com>
2026-04-28 00:31:22 +0300

    ppp

--- backend/app/routers/irm.py
+++ backend/app/routers/irm.py
@@ -12,7 +12,7 @@ from bson import ObjectId
 
 from app.models.documents import IRMScan, Patient, VisiteClinique
 from app.core.auth import get_current_user
-from app.core.database import get_db
+ 
 
 router = APIRouter()
 viewer_router = APIRouter()
